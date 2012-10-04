06328bd145da1aca87e1918bda18e7d980dd063c
spousty <spousty@redhat.com>
2012-10-04 14:02:12 -0400

    changing to better server.js

--- server.js
+++ server.js
@@ -1,69 +1,95 @@
 #!/bin/env node
-//  OpenShift sample Node application
 
 var express = require('express');
 var fs      = require('fs');
+var mongodb = require('mongodb');
 
-//  Local cache for static content [fixed and loaded at startup]
-var zcache = { 'index.html': '' };
-zcache['index.html'] = fs.readFileSync('./index.html'); //  Cache index.html
+var App = function(){
 
-// Create "express" server.
-var app  = express.createServer();
+  // Scope
 
+  var self = this;
 
-/*  =====================================================================  */
-/*  Setup route handlers.  */
-/*  =====================================================================  */
+  // Setup
 
-// Handler for GET /health
-app.get('/health', function(req, res){
-    res.send('1');
-});
+  self.zcache = { 'index.html': '' };
+  self.zcache['index.html'] = fs.readFileSync('./index.html');
+  
+  self.dbServer = new mongodb.Server(process.env.OPENSHIFT_NOSQL_DB_HOST, parseInt(process.env.OPENSHIFT_NOSQL_DB_PORT));
+  self.db = new mongodb.Db(process.env.OPENSHIFT_APP_NAME, self.dbServer, {auto_reconnect: true});
+  self.dbUser = process.env.OPENSHIFT_NOSQL_DB_USERNAME;
+  self.dbPass = process.env.OPENSHIFT_NOSQL_DB_PASSWORD;
 
-// Handler for GET /asciimo
-app.get('/asciimo', function(req, res){
+  self.ipaddr  = process.env.OPENSHIFT_INTERNAL_IP;
+  self.port    = parseInt(process.env.OPENSHIFT_INTERNAL_PORT) || 8080;
+
+  if (typeof self.ipaddr === "undefined") {
+    console.warn('No OPENSHIFT_INTERNAL_IP environment variable');
+  };
+
+  // Web app logic
+
+  self.routes = {};
+
+  self.routes['health'] = function(req, res){ res.send('1'); };
+
+  self.routes['asciimo'] = function(req, res){
     var link="https://a248.e.akamai.net/assets.github.com/img/d84f00f173afcf3bc81b4fad855e39838b23d8ff/687474703a2f2f696d6775722e636f6d2f6b6d626a422e706e67";
     res.send("<html><body><img src='" + link + "'></body></html>");
-});
+  };
 
-// Handler for GET /
-app.get('/', function(req, res){
-    res.send(zcache['index.html'], {'Content-Type': 'text/html'});
-});
+  self.routes['root'] = function(req, res){
+    self.db.collection('names').find().toArray(function(err, names) {
+        res.header("Content-Type:","text/json");
+        res.end(JSON.stringify(names));
+    });
+  };
 
 
-//  Get the environment variables we need.
-var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP;
-var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+  // Webapp
 
-if (typeof ipaddr === "undefined") {
-   console.warn('No OPENSHIFT_INTERNAL_IP environment variable');
-}
+  self.app  = express.createServer();
+  self.app.get('/health', self.routes['health']);
+  self.app.get('/asciimo', self.routes['asciimo']);
+  self.app.get('/', self.routes['root']);
 
-//  terminator === the termination handler.
-function terminator(sig) {
-   if (typeof sig === "string") {
-      console.log('%s: Received %s - terminating Node server ...',
-                  Date(Date.now()), sig);
+  // Logic
+
+  self.connectDb = function(callback){
+    self.db.open(function(err, db){
+      if(err){ throw err };
+      self.db.authenticate(self.dbUser, self.dbPass, function(err, res){
+        if(err){ throw err };
+        callback();
+      });
+    });
+  };
+
+  self.startServer = function(){
+    self.app.listen(self.port, self.ipaddr, function(){
+      console.log('%s: Node server started on %s:%d ...', Date(Date.now()), self.ipaddr, self.port);
+    });
+  }
+
+  // Destructors
+
+  self.terminator = function(sig) {
+    if (typeof sig === "string") {
+      console.log('%s: Received %s - terminating Node server ...', Date(Date.now()), sig);
       process.exit(1);
-   }
-   console.log('%s: Node server stopped.', Date(Date.now()) );
-}
+    };
+    console.log('%s: Node server stopped.', Date(Date.now()) );
+  };
 
-//  Process on exit and signals.
-process.on('exit', function() { terminator(); });
+  process.on('exit', function() { self.terminator(); });
 
-// Removed 'SIGPIPE' from the list - bugz 852598.
-['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
- 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-].forEach(function(element, index, array) {
-    process.on(element, function() { terminator(element); });
-});
+  self.terminatorSetup = function(element, index, array) {
+    process.on(element, function() { self.terminator(element); });
+  };
 
-//  And start the app on that interface (and port).
-app.listen(port, ipaddr, function() {
-   console.log('%s: Node server started on %s:%d ...', Date(Date.now() ),
-               ipaddr, port);
-});
+  ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'].forEach(self.terminatorSetup);
 
+};
+
+var app = new App();
+app.connectDb(app.startServer);
