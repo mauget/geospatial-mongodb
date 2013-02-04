3e84ce32a026b6bd77b119c324c4f80cd5fc65e6
L.E. Mauget <mauget@gmail.com>
2013-02-03 20:47:15 -0500

    Accumulate result prior to send

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var App = function(){
 	var query = {'loc': {$near: [ -73.977842, 40.752315 ]}};
 	
     self.db.collection( self.coll ).find( query ).limit(10).toArray(function(err, locations) {
-		res.header("Content-Type:","text/html");
+		//res.header("Content-Type:","text/html");
 		var s = "Nothing found";
 		if (locations != "undefined") {
 			s = '';
