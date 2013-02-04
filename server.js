61e3db09cfeb7c0eeb16f2e1916b777e49a85eaa
L.E. Mauget <mauget@gmail.com>
2013-02-03 19:48:19 -0500

    Near query

--- server.js
+++ server.js
@@ -35,11 +35,11 @@ var App = function(){
 	var arg = '95123';
 	var query = {zip: arg};
 	
-	var center = center = [-73.977842, 40.752315];
-	var radius = 2.0;
-	query = {loc: {$within: {$center: [ center, radius ] }}};
+	//var center = center = [-73.977842, 40.752315];
+	//var radius = 2.0;
+	query = {'loc': {$near: [ -73.977842, 40.752315 ]}};
 	
-    self.db.collection( self.coll ).find( query ).toArray(function(err, locations) {
+    self.db.collection( self.coll ).find( query ).limit(10).toArray(function(err, locations) {
 		//	res.header("Content-Type:","text/html");
 		var s = "Nothing found";
 		if (locations != "undefined") {
