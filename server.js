fa2bc584a6c85dc55b84fd70bb649a9e3e241db0
L.E. Mauget <mauget@gmail.com>
2013-02-03 22:01:48 -0500

    Fixed typeo on comparision

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ var App = function(){
 			for (var i = 0; i < locations.length; i++) {
 				var rec = locations[i];
 				s += '<li>';
-				s += '<p>City, state '+rec.city+', '+rec.state+' '+rec.zip + ' ';
+				s += 'City, state '+rec.city+', '+rec.state+' '+rec.zip + ' ';
 				s += 'Lat, long ('+rec.loc.x+','+rec,loc.y+')</li>';
 			}
 			s += '</ol>';
