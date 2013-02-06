12d3e86aad9b82ee5c77ef492b405dd15b3a9b73
L.E. Mauget <mauget@gmail.com>
2013-02-05 21:57:27 -0500

    Two-step query

--- server.js
+++ server.js
@@ -43,14 +43,13 @@ var App = function(){
 	//	self.query = decodeURIComponent(param);
 	//}
 	
-	var query = {'loc': {$near: [ -73.977842, 40.752315 ] } };
+	//var query = {'loc': {$near: [ -73.977842, 40.752315 ] } };
 	
 	self.db.collection( self.coll ).find( {zip: '27526'}).toArray(function(err, center) {
 		var rec = center[0];
 		var x = -rec.loc.x;
 		var y =  rec.loc.y;
 		query = {'loc': {$near: [ x, y ] } };
-	});
 	
 	
     self.db.collection( self.coll ).find( query ).limit( limit ).toArray(function(err, locations) {
@@ -68,6 +67,8 @@ var App = function(){
 			res.send(s);
 		}
     });
+	
+	});
 
   };
 
