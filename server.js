35b4842c8df524c1d61bc9f8fa7dad157c9dbc98
L.E. Mauget <mauget@gmail.com>
2013-02-06 21:46:44 -0500

    Two-step query

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ var App = function(){
 	//var center = center = [-73.977842, 40.752315];
 	//var radius = 2.0;
 	var limit = 25;
-	
+
 	//var param = req.query.query;
 	//if (param !== 'undefined'){
 	//	self.query = decodeURIComponent(param);
