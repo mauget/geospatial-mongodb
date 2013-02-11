f921110c364220fc3e30987f7a10d5e3e65b92f1
L.E. Mauget <mauget@gmail.com>
2013-02-10 19:58:49 -0500

    Search for cities

--- server.js
+++ server.js
@@ -169,7 +169,7 @@ var NodeApp = function() {
 			if (!like) {
 				res.send('{err: "Bad input"}');
 			} else {
-				// db.zips.find({city: {$regex: '^fuquay.*', $options: 'i' }}  )
+				// db.zips.find({city: {$regex: '^.*fuquay.*', $options: 'i' }}  )
 				var query = {city: { $regex: ('^.*%.*', like), $options: 'i' } };
 			
 				self.db.collection( self.coll ).find( query ).limit( limit ).toArray( function( err, cities ) {
