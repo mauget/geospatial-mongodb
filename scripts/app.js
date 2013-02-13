65a9229da77f7971772cece16ba10261ef647a32
L.E. Mauget <mauget@gmail.com>
2013-02-12 21:09:23 -0500

    Scripting on client side

--- scripts/app.js
+++ scripts/app.js
@@ -14,14 +14,18 @@
 		self.nearZip = function() {
 		    
 			$.getJSON("/near/zip/27526", function(data) {
-				console.log(JSON.stringify(data));
+				//console.log(JSON.stringify(data));
+				
+				$.each(data, function(index, val) {
+					console.log('%s. %s, %s %s', index, val.city, val.state, val.zip);
+				});
 			});
 		};
 
 		self.citiesVarina = function() {
 			
 			$.getJSON("/cities/varina", function(data) {
-				console.log(JSON.stringify(data));
+				//console.log(JSON.stringify(data));
 				
 				$.each(data, function(index, val) {
 					console.log('%s. %s, %s %s', index, val.city, val.state, val.zip);
@@ -32,15 +36,11 @@
 		self.nearLatLon = function() {
 			
 			$.getJSON("/near/lat/35.579952/lon/78.790807", function(data) {
-				console.log(JSON.stringify(data));
+				//console.log(JSON.stringify(data));
 				var items = jQuery.parseJSON(data);
-			
-				$.each(items, function(i,rec) {
-					$('#nearList1').html(rec.city);
-					$('#nearList2').html(rec.state);
-					$('#nearList3').html(rec.zip);
-					$('#nearList4').html(rec.loc.y);
-					$('#nearList5').html(rec.loc.x);
+							
+				$.each(data, function(index, val) {
+					console.log('%s. %s, %s %s', index, val.city, val.state, val.zip);
 				});
 			});
 		};
