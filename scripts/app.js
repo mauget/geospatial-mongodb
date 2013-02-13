2e95c8f6cfa01798c4c7fe674dc9874e1cf5fa3f
L.E. Mauget <mauget@gmail.com>
2013-02-12 21:39:46 -0500

    Scripting on client side

--- scripts/app.js
+++ scripts/app.js
@@ -36,7 +36,8 @@
 		};
 		
 		self.createRow = function(index, val) {
-			var row = ('<div class="ui-block-a">%s</div><div class="ui-block-b">%s</div><div class="ui-block-c">%s</div><div class="ui-block-d">%s</div><div class="ui-block-e">(%s,%s)</div>, index, val.city, val.state, val.zip, val.loc.y, val.loc.x);
+			var row = ('<div class="ui-block-a">%s</div><div class="ui-block-b">%s</div><div class="ui-block-c">%s</div><div class="ui-block-d">%s</div><div class="ui-block-e">(%s,%s)</div>', index, val.city, val.state, val.zip, val.loc.y, val.loc.x);
+			return row;
 		};
 
 		self.nearLatLon = function() {
