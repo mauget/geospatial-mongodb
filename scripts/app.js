af544e572779666d5bc56ae2396b11246415d4c7
L.E. Mauget <mauget@gmail.com>
2013-02-14 21:58:27 -0500

    Scripting on client side

--- scripts/app.js
+++ scripts/app.js
@@ -40,7 +40,7 @@
 		
 		self.createRow = function(index, val) {
 		
-			var row = '<div class="ui-block-a">%s0</div><div class="ui-block-b">%s1</div><div class="ui-block-c">%s2&nbsp;&nbsp;%s3</div>';
+			var row = '<div class="ui-block-a">%s0&nbsp;%s1</div><div class="ui-block-b">%s2&nbsp;&nbsp;%s3</div>';
 			
 			row = row.replace('%s0', Number(index) + 1);
 			row = row.replace('%s1', val.city);
