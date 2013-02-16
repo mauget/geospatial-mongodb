cf9d8807af13e189ceda61cb5a3afb7d31b29f84
L.E. Mauget <mauget@gmail.com>
2013-02-16 17:26:11 -0500

    Jade

--- scripts/app.js
+++ scripts/app.js
@@ -35,16 +35,6 @@
 			});
 		};
 		
-		self.createRow = function(index, val) {
-			
-			var row = '<div class="ui-block-a">%s0&nbsp;%s1</div><div class="ui-block-b">%s2&nbsp;&nbsp;%s3</div>';
-			row = row.replace('%s0', Number(index) + 1)
-				.replace('%s1', val.city)
-				.replace('%s2', val.state)
-				.replace('%s3', val.zip);
-			return row;
-		};
-
 		self.nearLatLon = function() {
 			
 			$.getJSON("/near/lat/35.579952/lon/78.790807", function(data) {
@@ -55,6 +45,23 @@
 					console.log(row);
 				});
 			});
+		};
+
+		
+		self.renderRow = function(index, val) {
+				var row = self.createRow(index, val);
+				$(row).appendTo('#nearList');
+				console.log(row);
+		}
+		
+		self.createRow = function(index, val) {
+			
+			var row = '<div class="ui-block-a">%s0&nbsp;%s1</div><div class="ui-block-b">%s2&nbsp;&nbsp;%s3</div>';
+			row = row.replace('%s0', Number(index) + 1)
+				.replace('%s1', val.city)
+				.replace('%s2', val.state)
+				.replace('%s3', val.zip);
+			return row;
 		};
 
 	};
