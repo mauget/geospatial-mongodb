6cc5b7f186a022e0d653aa07ccad53f1308a6ef6
L.E. Mauget <mauget@gmail.com>
2013-02-16 19:58:17 -0500

    Jade

--- scripts/app.js
+++ scripts/app.js
@@ -10,40 +10,45 @@
 
 		self.nearZip = function() {   
 			$.getJSON("/near/zip/27526", function(data) {
-				//console.log(JSON.stringify(data));
+				var markup = '';
+				$.each(data, function(index, val) {self.renderRow(index, val, markup);});
 				$('#nearList').html('');
-				$.each(data, function(index, val) {self.renderRow(index, val);});
+				$('#nearList').append(markup);
 				$('#nearList').listview("refresh");
 			});
 		};
 
 		self.citiesVarina = function() {	
 			$.getJSON("/cities/varina", function(data) {
-				$('#nearList').html('');	
-				$.each(data, function(index, val) {self.renderRow(index, val);});
+				var markup = '';
+				$.each(data, function(index, val) {self.renderRow(index, val, markup);});
+				$('#nearList').html('');
+				$('#nearList').append(markup);
 				$('#nearList').listview("refresh");
 			});
 		};
 		
 		self.nearLatLon = function() {	
-			$.getJSON("/near/lat/35.579952/lon/78.790807", function(data) {
-				$('#nearList').html('');	
-				$.each(data, function(index, val) {self.renderRow(index, val);});
-				console.log($('#nearList').html());
+			$.getJSON("/near/lat/35.579952/lon/78.790807", function(data) {	
+				var markup = '';
+				$.each(data, function(index, val) {self.renderRow(index, val, markup);});
+				$('#nearList').html('');
+				$('#nearList').append(markup);
 				$('#nearList').listview("refresh");
 			});
 		};
 		
-		self.renderRow = function(index, val) {
+		self.renderRow = function(index, val, markup) {
 				var row = self.createRow(index, val);
-				$(row).appendTo('#nearList');
+				markup.push(row);
+				//$(row).appendTo('#nearList');
 				//console.log(row);
 				//console.log('%s. %s, %s %s (%s,%s)', index, val.city, val.state, val.zip, val.loc.y, val.loc.x);
 		}
 		
 		self.createRow = function(index, val) {	
 			var row = '<li><a href=".">%s1&nbsp;%s2&nbsp;&nbsp;%s3</a><li>';
-			return row;//.replace('%s1', val.city).replace('%s2', val.state).replace('%s3', val.zip);
+			return row.replace('%s1', val.city).replace('%s2', val.state).replace('%s3', val.zip);
 		};
 
 	};
