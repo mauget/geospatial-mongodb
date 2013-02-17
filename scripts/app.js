ff7f781d2cb026c40f9cbd270d38a3a3e1537682
L.E. Mauget <mauget@gmail.com>
2013-02-17 17:27:41 -0500

    Li

--- scripts/app.js
+++ scripts/app.js
@@ -7,9 +7,7 @@ $(document).ready(function() {
 		var txtIn = $('#citySearch').val();
 		txtIn = APP.trim(txtIn);
 		console.log(txtIn);
-		
-		$('#cityList').html('');
-		
+				
 		if (txtIn.length > 0){
 		
 			// REST: search
@@ -35,35 +33,36 @@ APP = new function() {
 		});
 	};
 
-		self.citiesVarina = function() {	
-			$.getJSON("/cities/varina", function(data) {
-				self.renderList('#nearList', data);
-			});
-		};
+	self.citiesVarina = function() {	
+		$.getJSON("/cities/varina", function(data) {
+			self.renderList('#nearList', data);
+		});
+	};
 		
-		self.nearLatLon = function() {	
-			$.getJSON("/near/lat/35.579952/lon/78.790807", function(data) {	
-				self.renderList('#nearList', data);
-			});
-		};
+	self.nearLatLon = function() {	
+		$.getJSON("/near/lat/35.579952/lon/78.790807", function(data) {	
+			self.renderList('#nearList', data);
+		});
+	};
 		
-		self.renderList = function(listSelector, data) {
-			var markup = [];
-			$.each(data, function(index, val) {self.renderRow(index, val, markup);});
-			$(listSelector).html(markup);
-			$(listSelector).listview('refresh');
-		}
+	self.renderList = function(listSelector, data) {	
+		//$(listSelector).html('');
+		var markup = [];
+		$.each(data, function(index, val) {self.renderRow(index, val, markup);});
+		$(listSelector).html(markup);
+		$(listSelector).listview('refresh');
+	}
 		
-		self.renderRow = function(index, val, markup) {
-				var row = self.createRow(index, val);
-				markup.push(row);
-				//console.log(row);
-				//console.log('%s. %s, %s %s (%s,%s)', index, val.city, val.state, val.zip, val.loc.y, val.loc.x);
-		}
+	self.renderRow = function(index, val, markup) {
+		var row = self.createRow(index, val);
+		markup.push(row);
+		//console.log(row);
+		//console.log('%s. %s, %s %s (%s,%s)', index, val.city, val.state, val.zip, val.loc.y, val.loc.x);
+	}
 		
-		self.createRow = function(index, val) {	
-			var row = '<li><a href=".">%s1&nbsp;%s2&nbsp;&nbsp;%s3</a></li>';
-			return row.replace('%s1', val.city).replace('%s2', val.state).replace('%s3', val.zip);
-		};
+	self.createRow = function(index, val) {	
+		var row = '<li><a href=".">%s1&nbsp;%s2&nbsp;&nbsp;%s3</a></li>';
+		return row.replace('%s1', val.city).replace('%s2', val.state).replace('%s3', val.zip);
+	};
 
 };
