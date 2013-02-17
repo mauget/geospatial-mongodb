925148d9a5a65b3392b745ba0fb6276837c686cc
L.E. Mauget <mauget@gmail.com>
2013-02-16 21:10:47 -0500

    Li

--- scripts/app.js
+++ scripts/app.js
@@ -27,21 +27,21 @@
 		};
 		
 		self.renderList = function(listSelector, data) {
-			var markup = [''];
-		//	$.each(data, function(index, val) {self.renderRow(index, val, markup);});
-			$(listSelector).html($.each(data, function(index, val) {self.renderRow(index, val, markup);}));
+			var markup = [];
+			$.each(data, function(index, val) {self.renderRow(index, val, markup);});
+			$(listSelector).html(markup);
 			$(listSelector).listview('refresh');
 		}
 		
 		self.renderRow = function(index, val, markup) {
 				var row = self.createRow(index, val);
-				markup[0] = markup[0] + row;
+				markup.push(row);
 				//console.log(row);
 				//console.log('%s. %s, %s %s (%s,%s)', index, val.city, val.state, val.zip, val.loc.y, val.loc.x);
 		}
 		
 		self.createRow = function(index, val) {	
-			var row = '<li><a href=".">%s1&nbsp;%s2&nbsp;&nbsp;%s3</a><li>';
+			var row = '<li><a href=".">%s1&nbsp;%s2&nbsp;&nbsp;%s3</a></li>';
 			return row.replace('%s1', val.city).replace('%s2', val.state).replace('%s3', val.zip);
 		};
 
