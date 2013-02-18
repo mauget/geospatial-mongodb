9a1d6703a79a4a64eee8b89a8ce83c4ba1af081a
L.E. Mauget <mauget@gmail.com>
2013-02-18 10:51:12 -0500

    map

--- scripts/app.js
+++ scripts/app.js
@@ -98,6 +98,12 @@ $(document).ready(function() {
 
 	// Listen for city search clicks from now-on
 	APP.bindCitySearch();
+	
+	$('#map_canvas').gmap().bind('init', function(ev, map) {
+		$('#map_canvas').gmap('addMarker', {'position': '57.7973333,12.0502107', 'bounds': true}).click(function() {
+			$('#map_canvas').gmap('openInfoWindow', {'content': 'Hello World!'}, this);
+		});
+	});
 
 });	
 
