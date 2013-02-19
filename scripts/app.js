bfefbdd9d0d1008e72fb578c1bf1e6b8ba62bd9c
L.E. Mauget <mauget@gmail.com>
2013-02-18 19:01:04 -0500

    map

--- scripts/app.js
+++ scripts/app.js
@@ -45,9 +45,10 @@ $(document).ready(function() {
 			$(listSelector).listview('refresh');
 			self.bindNearSearch();
 				
-			var latlon = { 'y': Number( data[0].loc.y ), 'x': -Number( data[0].loc.x ) };
-		//	console.log(JSON.stringify(latlon));
-		//	$('#map_canvas').gmap({'center': latlon});
+			var ll = { 'y': Number( data[0].loc.y ), 'x': -Number( data[0].loc.x ) };
+			var latlon = new google.maps.LatLng(ll.y, ll.x);
+			console.log(JSON.stringify(ll));
+			$('#map_canvas').gmap({'center': latlon});
 		}
 	
 		self.renderRow = function(index, val, markup) {
