5d98ffd9046a6c08aae48b5f13fe6ed21684cc55
L.E. Mauget <mauget@gmail.com>
2013-02-18 20:34:59 -0500

    map

--- scripts/app.js
+++ scripts/app.js
@@ -87,7 +87,7 @@ $(document).ready(function() {
 			var latLon = template.replace('%s1', data[0].loc.y).replace('%s2', data[0].loc.x);
 	
 			console.log(latLon);
-	
+			$('#map_canvas').gmap('destroy');
 			$('#two').live('pageshow', function() {
 				$('#map_canvas').gmap('refresh');
 			});
