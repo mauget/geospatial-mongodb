3deffbfb7a2684294e99c04884048a564764feda
L.E. Mauget <mauget@gmail.com>
2013-02-23 18:40:35 -0500

    map

--- scripts/app.js
+++ scripts/app.js
@@ -100,26 +100,26 @@ $(document).ready(function() {
 				latLon = template.replace('%s1', m.loc.y).replace('%s2', m.loc.x);
 				// $('#map_canvas').gmap( 'addMarker', { 'position': latLon, 'bounds': true } );
 				
-			//	$('#map_canvas').gmap( { 'callback': function() {
+				$('#map_canvas').gmap( { 'callback': function() {
 					var thisMap = $('#map_canvas');
 					thisMap.gmap( 'addMarker', { 'position': latLon, 'bounds': true } ).click( function() {
 						thisMap.openInfoWindow( { 'content': zipText }, this);
 					});
-			//	}});
+				} } ); 
 				
 				/*
 				$('#map_canvas').gmap( 'addMarker', { 'position': latLon, 'bounds': true } ).click( function() {
 					thisMap.openInfoWindow( { 'content': zipText }, this);
 				});
 				*/
-			/*	
+				/*	
 				$('#map_canvas').gmap( { 'callback': function() {
 					var thisMap = this; 
 					thisMap.addMarker( { 'position': latLon, 'bounds': true } ).click( function() {
 						thisMap.openInfoWindow( { 'content': zipText }, this);
 					});
 				}});
-			*/	
+				*/	
 			});
 				
 			self.clearSearch();
