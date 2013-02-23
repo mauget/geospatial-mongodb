82d1db21a067e7967e71ac238237666f23df873e
L.E. Mauget <mauget@gmail.com>
2013-02-23 11:28:32 -0500

    map

--- scripts/app.js
+++ scripts/app.js
@@ -96,8 +96,17 @@ $(document).ready(function() {
 			$('#map_canvas').gmap( { 'center': latLon, 'zoom': 8 } );
 			
 			$.each( data, function(i, m) {
+				var zip = m.zip;
 				latLon = template.replace('%s1', m.loc.y).replace('%s2', m.loc.x);
-				$('#map_canvas').gmap( 'addMarker', { 'position': latLon, 'title': m.zip, 'zIndex': i } );
+				// $('#map_canvas').gmap( 'addMarker', { 'position': latLon, 'bounds': true } );
+				
+				$('#map_canvas').gmap({'callback': function() {
+					var thisMap = this; 
+					thisMap.addMarker({'position': latLon, 'bounds': true}).click(function() {
+						thisMap.openInfoWindow({'content': 'ZipCode ' + zip}, this);
+					});
+				}});
+				
 			});
 				
 			self.clearSearch();
