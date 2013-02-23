88246e1b671c8f4b5429a3840faff62dac2a2049
L.E. Mauget <mauget@gmail.com>
2013-02-23 11:49:13 -0500

    map

--- scripts/app.js
+++ scripts/app.js
@@ -99,14 +99,17 @@ $(document).ready(function() {
 				var zipText = 'ZipCode ' + m.zip;
 				latLon = template.replace('%s1', m.loc.y).replace('%s2', m.loc.x);
 				// $('#map_canvas').gmap( 'addMarker', { 'position': latLon, 'bounds': true } );
-				
+				$('#map_canvas').gmap( 'addMarker', { 'position': latLon, 'bounds': true } ).click( function() {
+					thisMap.openInfoWindow( { 'content': zipText }, this);
+				});
+			/*	
 				$('#map_canvas').gmap( { 'callback': function() {
 					var thisMap = this; 
 					thisMap.addMarker( { 'position': latLon, 'bounds': true } ).click( function() {
 						thisMap.openInfoWindow( { 'content': zipText }, this);
 					});
 				}});
-				
+			*/	
 			});
 				
 			self.clearSearch();
