f09496eb77f1c03d7212856ffa5f3e2d772fb975
L.E. Mauget <mauget@gmail.com>
2013-02-22 20:05:37 -0500

    map

--- scripts/app.js
+++ scripts/app.js
@@ -98,6 +98,9 @@ $(document).ready(function() {
 				latLon = template.replace('%s1', m.loc.y).replace('%s2', m.loc.x);
 				$('#map_canvas').gmap( 'addMarker', { 'position': latLon, 'title': m.zip, 'zIndex': i } );
 			});
+			
+			// Clear search
+			$('#cityInput').text = '';
 		
 		}
 
