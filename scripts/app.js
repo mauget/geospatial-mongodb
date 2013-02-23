079f958ad7443bf8e31108938e7f672e7a59fea2
L.E. Mauget <mauget@gmail.com>
2013-02-23 18:59:10 -0500

    map

--- scripts/app.js
+++ scripts/app.js
@@ -103,7 +103,7 @@ $(document).ready(function() {
 				$('#map_canvas').gmap().bind('init', function(ev, map) {
 				
 					$('#map_canvas').gmap('addMarker, { 'position': latLon, 'bounds': true } ).click(function() {
-						$('#map_canvas').gmap( 'openInfoWindow', {'content': zipText}, this);
+	//					$('#map_canvas').gmap( 'openInfoWindow', {'content': zipText }, this);
 					});
 					
 				});
