fc34dd73d565551dc11f2c5f162c6b2a8eea1845
L.E. Mauget <mauget@gmail.com>
2013-02-22 19:37:21 -0500

    map

--- scripts/app.js
+++ scripts/app.js
@@ -96,7 +96,7 @@ $(document).ready(function() {
 			
 			$.each( data, function(i, m) {
 				latLon = template.replace('%s1', m.loc.y).replace('%s2', m.loc.x);
-				$('#map_canvas').gmap( 'addMarker', { 'position': latLon } );
+				$('#map_canvas').gmap( 'addMarker', { 'position': latLon, 'title': m.zip } );
 			});
 		
 		}
@@ -118,7 +118,7 @@ $(document).ready(function() {
 		};
 	};
 
-	// Listen for city search clicks from now-on
+	// Listen for city search clicks for duration of life-cycle
 	APP.bindCitySearch();
 
 });	
