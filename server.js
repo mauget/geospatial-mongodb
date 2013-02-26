aa96b8a83e5f2348ef4cbad9972be1e4baefb114
L.E. Mauget <mauget@gmail.com>
2013-02-26 18:00:59 -0500

    like regexp

--- server.js
+++ server.js
@@ -181,7 +181,7 @@ var NodeApp = function() {
 				res.send('{err: "Bad input"}');
 			} else {
 				// Regexp wildcard suffix query
-				var query = { city: { $regex: ('^%s.*$', like), $options: '' } };
+				var query = { city: { $regex: ('^%s.*$', like) } };
 			
 				self.db.collection( self.coll ).find( query ).limit( limit ).toArray( function( err, cities ) {
 					if (!cities ) {
